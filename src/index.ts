11bb473880a838b19ea1c32c459599e324fd6755
fernando <ffoliveres@gmail.com>
2024-08-10 18:11:14 +0200

    Modified index.ts by Fernando

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 console.log("Hello world");
-console.log("This is another line");
+console.log("This is another line. This text has been modified by Fernando");
 console.log("This is yet another newest line");
+console.log("Also, this console log is a new one by me(Fernando)")
